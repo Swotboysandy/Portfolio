e1d96f3d8c90c2178ebf2b6fda90d9120e35922e
Swotboysandy <swotboysandy@gmail.com>
2026-01-08 02:13:11 +0530

    Add remaining components and utilities

---
+++ src/app/components/divider/index.tsx
@@ -0,0 +1,10 @@
+
+const Divider = () => {
+    return (
+        <div className="container">
+            <div className="h-2 border border-primary/10" />
+        </div>
+    )
+}
+
+export default Divider
